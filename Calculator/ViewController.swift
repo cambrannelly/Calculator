db9b1cf1426710247cfc81d054deda9a41f4068e
Cam Brannelly <cambrannelly@Cams-MacBook-Pro.local>
2018-05-26 11:54:47 -0600

    update

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let pickerData = ["+", "-", "*", "/"]
+    @IBOutlet weak var operatorPicker: UIPickerView!
+    @IBOutlet weak var secondOperand: UITextField!
+    @IBOutlet weak var firstOperand: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        operatorPicker.delegate = self
+        operatorPicker.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapPressed(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
 }
 
